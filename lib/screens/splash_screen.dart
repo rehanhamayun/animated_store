2d4d8449a6c72f1a9f7786a39fc118e9fb4d96cf
Rehan Hamayun <rr8329203@gmail.com>
2022-02-20 14:38:36 +0500

    fix issues

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:google_login/core/animations/animations.dart';
+import 'package:google_login/core/utils/utils.dart';
+
+import 'home_screen/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    _next();
+    super.initState();
+  }
+
+  _next() {
+    Timer(
+      const Duration(milliseconds: 450),
+      () {
+        Navigation.push(
+          context,
+          customPageTransition: PageTransition(
+            child: const HomeScreen(),
+            type: PageTransitionType.fadeIn,
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Container(),
+    );
+  }
+}
